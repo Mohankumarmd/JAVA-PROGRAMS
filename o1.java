ca6a4dbfa362c7bfaa2298ccf078201c2cfd10ae
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-05 09:39:28 +0530

    Added

--- o1.java
+++ o1.java
@@ -109,3 +109,46 @@ public class AddNumbers {
         System.out.println("Sum = " + sum);
     }
 }
+
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("Enter three numbers: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        if (a >= b && a >= c)
+            System.out.println("Largest number is " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Largest number is " + b);
+        else
+            System.out.println("Largest number is " + c);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
+
+
