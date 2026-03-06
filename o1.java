d5a99a693e1e2f08375946d92a780b0f5e3ff136
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-06 09:28:42 +0530

    Add

--- o1.java
+++ o1.java
@@ -170,3 +170,22 @@ public class Palindrome {
 }
 
 
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 7;
+        boolean prime = true;
+
+        for(int i = 2; i <= num/2; i++) {
+            if(num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+}
+
